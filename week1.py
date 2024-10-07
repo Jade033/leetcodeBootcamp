a343bdae3770e3deb3166af79340989ede5de66d
Jade033 <easonxty@gmail.com>
2024-10-07 12:27:46 -0400

    Update week1.py

--- week1.py
+++ week1.py
@@ -10,7 +10,23 @@ class Solution:
             else:
                 end -= 1 
 
-    def question2():
-        pass
+    def productExceptSelf(self, nums):
+        leftPro = 1
+        rightPro = 1
+        result = [1]*len(nums)
+        leftInd = 1
+        rightInd = len(result) - 2
+        leftCursor = 0
+        rightCursor = len(nums) - 1
+        while leftInd < len(result):
+            leftPro *= nums[leftCursor]
+            rightPro *= nums[rightCursor]
+            leftCursor += 1
+            rightCursor -= 1
+            result[leftInd] *= leftPro
+            result[rightInd] *= rightPro
+            leftInd += 1
+            rightInd -= 1
+        return result
 
     
